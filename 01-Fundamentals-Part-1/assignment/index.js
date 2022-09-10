bce48035298d87d227bbe78c69202135802b077c
lugfigue <lugfigue@bancolombia.com.co>
2022-09-10 09:43:46 -0500

    c

--- 01-Fundamentals-Part-1/assignment/index.js
+++ 01-Fundamentals-Part-1/assignment/index.js
@@ -1,3 +1,10 @@
 const country = "Colombia";
 const continent = "SA";
-const population = 422500000;
+const population = 422_500_000;
+const isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
